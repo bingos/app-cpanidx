f619eba3589df527ea7fdfc04a0170da687835fd
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-07-14 13:41:56 +0100

    Make the 'url' parameter optional for the cpanidx-gendb script

--- lib/App/CPANIDX.pm
+++ lib/App/CPANIDX.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use vars qw($VERSION);
 
-$VERSION = '0.18';
+$VERSION = '0.20';
 
 1;
 
--- lib/App/CPANIDX/Tables.pm
+++ lib/App/CPANIDX/Tables.pm
@@ -5,7 +5,7 @@ use warnings;
 use Module::CoreList::DBSchema;
 use vars qw[$VERSION];
 
-$VERSION = '0.18';
+$VERSION = '0.20';
 
 my $mcdbs = Module::CoreList::DBSchema->new();
 my %cl_tables = $mcdbs->tables();
