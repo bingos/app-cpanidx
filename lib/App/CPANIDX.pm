a505b717f380bf26ab6b39e01e9a72140982994c
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-07-27 22:16:04 +0100

    Bump version to 0.22

--- lib/App/CPANIDX.pm
+++ lib/App/CPANIDX.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use vars qw($VERSION);
 
-$VERSION = '0.20';
+$VERSION = '0.22';
 
 1;
 
--- lib/App/CPANIDX/Tables.pm
+++ lib/App/CPANIDX/Tables.pm
@@ -5,7 +5,7 @@ use warnings;
 use Module::CoreList::DBSchema;
 use vars qw[$VERSION];
 
-$VERSION = '0.20';
+$VERSION = '0.22';
 
 my $mcdbs = Module::CoreList::DBSchema->new();
 my %cl_tables = $mcdbs->tables();
